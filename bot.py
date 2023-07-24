eb24eb639a185ca112d5118fce9d599ada155411
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-24 12:56:20 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,5 @@
 import telegram
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackQueryHandler
 import yt_dlp
 
 # Replace this with your bot's token
@@ -29,30 +29,42 @@ def get_dash_manifest_url(update, context):
         info_dict = ydl.extract_info(live_stream_url, download=False)
         formats = info_dict.get("formats")
 
-    # Create a list of quality levels
-    quality_levels = [f"{format['height']}p" for format in formats if format.get('height')]
+    # Create a list of quality levels with links as inline keyboard buttons
+    quality_buttons = []
+    for format in formats:
+        if format.get('height'):
+            quality_buttons.append([telegram.InlineKeyboardButton(f"{format['height']}p", callback_data=format['format_id'])])
 
-    # Prompt the user to select a quality level from the list
-    update.message.reply_text("Select a quality level from the list:\n\n" + "\n".join(quality_levels))
+    # Create an InlineKeyboardMarkup with the quality buttons
+    reply_markup = telegram.InlineKeyboardMarkup(quality_buttons)
+
+    # Prompt the user to select a quality level using the inline keyboard
+    update.message.reply_text("Select a quality level:", reply_markup=reply_markup)
 
     # Store the available quality levels in the context for the next state
-    context.user_data["quality_levels"] = quality_levels
+    context.user_data["quality_buttons"] = quality_buttons
 
     # Return QUALITY_SELECTION state to handle the user's response
     return QUALITY_SELECTION
 
 def handle_quality_selection(update, context):
     # Get the user's selected quality level
-    selected_quality = update.message.text
+    selected_quality = update.callback_query.data
 
-    # Get the available quality levels from the context
-    quality_levels = context.user_data.get("quality_levels", [])
+    # Get the available quality buttons from the context
+    quality_buttons = context.user_data.get("quality_buttons", [])
 
-    # Check if the selected quality is valid
-    if selected_quality in quality_levels:
+    # Find the selected quality in the buttons list to retrieve the corresponding URL
+    selected_url = None
+    for button_row in quality_buttons:
+        for button in button_row:
+            if button.callback_data == selected_quality:
+                selected_url = button.text
+
+    if selected_url:
         # Extract the dash manifest URL for the selected format
         ydl_opts = {
-            "format": f"bestvideo[height={selected_quality}]+bestaudio/best",
+            "format": selected_quality,
             "forcejson": True,
             "simulate": True,
             "quiet": True,
@@ -64,7 +76,7 @@ def handle_quality_selection(update, context):
             dash_manifest_url = info_dict.get("url")
 
         # Send the dash manifest URL back to the user
-        update.message.reply_text(f"Dash manifest URL ({selected_quality}): {dash_manifest_url}")
+        update.message.reply_text(f"Dash manifest URL ({selected_url}): {dash_manifest_url}")
     else:
         update.message.reply_text("Invalid quality selection. Please select a quality level from the list.")
 
@@ -81,11 +93,14 @@ def main():
     # Add a command handler to start the conversation
     updater.dispatcher.add_handler(CommandHandler("start", start))
 
+    # Add a callback query handler to handle inline keyboard button clicks
+    updater.dispatcher.add_handler(CallbackQueryHandler(handle_quality_selection))
+
     # Create a conversation handler to handle the quality selection flow
     quality_selection_handler = ConversationHandler(
         entry_points=[MessageHandler(Filters.text & ~Filters.command, get_dash_manifest_url)],
         states={
-            QUALITY_SELECTION: [MessageHandler(Filters.text & ~Filters.command, handle_quality_selection)]
+            QUALITY_SELECTION: [CallbackQueryHandler(handle_quality_selection)]
         },
         fallbacks=[]
     )
