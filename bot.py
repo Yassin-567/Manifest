72189c3404c8cb9b7ba66095b47faf9440a5b8ea
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-24 12:23:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -8,6 +8,10 @@ bot_token = "5987250865:AAHjt2KwXxLXg-szVMmfnfWdG6UA1hcgmwI"
 # Define the states used in the conversation flow
 QUALITY_SELECTION = 0
 
+def start(update, context):
+    # Send a welcome message with instructions to the user
+    update.message.reply_text("Hello, I am ready to extract the manifest URL.\n\nSend me a YouTube video link.")
+
 def get_dash_manifest_url(update, context):
     # Get the YouTube live stream URL from the message sent to the bot
     live_stream_url = update.message.text
@@ -25,34 +29,46 @@ def get_dash_manifest_url(update, context):
         info_dict = ydl.extract_info(live_stream_url, download=False)
         formats = info_dict.get("formats")
 
-    # Prompt the user to select a quality level
-    quality_levels = [f"{format['height']}p" for format in formats if format.get('height')]
-    prompt = "Select a quality level:\n\n" + "\n".join([f"{i+1}. {level}" for i, level in enumerate(quality_levels)])
-    update.message.reply_text(prompt)
+    # Create a list of quality levels with links as buttons
+    quality_buttons = []
+    for format in formats:
+        if format.get('height'):
+            quality_buttons.append(telegram.KeyboardButton(f"{format['height']}p", callback_data=format['format_id']))
 
-    # Define a function to handle the user's response
-    def handle_quality_selection(update, context):
-        # Get the user's selected quality level
-        selected_index = int(update.message.text) - 1
-        selected_format = formats[selected_index]
+    # Create a ReplyKeyboardMarkup with the quality buttons
+    reply_markup = telegram.ReplyKeyboardMarkup(
+        [quality_buttons],
+        one_time_keyboard=True,
+        resize_keyboard=True
+    )
 
-        # Extract the dash manifest URL for the selected format
-        ydl_opts["format"] = selected_format["format_id"]
-        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(live_stream_url, download=False)
-            dash_manifest_url = info_dict.get("url")
+    # Prompt the user to select a quality level using the buttons
+    update.message.reply_text("Select a quality level:", reply_markup=reply_markup)
 
-        # Send the dash manifest URL back to the user
-        update.message.reply_text(f"Dash manifest URL ({selected_format['height']}p): {dash_manifest_url}")
+    # Return QUALITY_SELECTION state to handle the user's response
+    return QUALITY_SELECTION
 
-        # End the conversation
-        return ConversationHandler.END
+def handle_quality_selection(update, context):
+    # Get the user's selected quality level
+    selected_format_id = update.callback_query.data
 
-    # Add a message handler to listen for the user's response
-    selection_handler = MessageHandler(Filters.regex(r"^[1-9][0-9]*$"), handle_quality_selection)
-    context.dispatcher.add_handler(selection_handler)
+    # Extract the dash manifest URL for the selected format
+    ydl_opts = {
+        "format": selected_format_id,
+        "forcejson": True,
+        "simulate": True,
+        "quiet": True,
+        "no_warnings": True,
+        "extract_flat": True
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info_dict = ydl.extract_info(update.message.text, download=False)
+        dash_manifest_url = info_dict.get("url")
 
-    # Return to the main event loop and wait for the user's response
+    # Send the dash manifest URL back to the user
+    update.message.reply_text(f"Dash manifest URL: {dash_manifest_url}")
+
+    # End the conversation
     return ConversationHandler.END
 
 def main():
@@ -62,14 +78,16 @@ def main():
     # Create an instance of the Telegram updater and attach the bot to it
     updater = Updater(token=bot_token, use_context=True)
 
+    # Add a command handler to start the conversation
+    updater.dispatcher.add_handler(CommandHandler("start", start))
+
     # Create a conversation handler to handle the quality selection flow
     quality_selection_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex(r"(http(s)?://)?((w){3}.)?youtu(be|.be)?(\.com)?/.+"), get_dash_manifest_url)],
+        entry_points=[MessageHandler(Filters.text & ~Filters.command, get_dash_manifest_url)],
         states={
-            QUALITY_SELECTION: [MessageHandler(Filters.regex(r"^[1-9][0-9]*$"), get_dash_manifest_url)]
+            QUALITY_SELECTION: [MessageHandler(Filters.text & ~Filters.command, handle_quality_selection)]
         },
-        fallbacks=[],
-        allow_reentry=True
+        fallbacks=[]
     )
 
     # Add the quality selection handler to the updater
