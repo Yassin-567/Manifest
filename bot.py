b3e23e258998cd7b714e978e9e011c6e8da334da
Yassin-567 <83673138+Yassin-567@users.noreply.github.com>
2023-07-26 14:31:24 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,15 @@
 import telegram
-from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
+from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+import yt_dlp
 
 # Replace this with your bot's token
-bot_token = "5987250865:AAHjt2KwXxLXg-szVMmfnfWdG6UA1hcgmwI"
+bot_token = "6266356161:AAEd7RPG1NnYlYr6rDikURVDzZGHtyozeA8"
 
 # Define the states used in the conversation flow
-QUALITY_SELECTION = 0
+QUALITY_SELECTION, GET_VIDEO_URL = range(2)
 
-def start(update, context):
+def start(update: telegram.Update, context: CallbackContext):
     """Handler function for the /start command"""
     # Define the custom keyboard
     keyboard = [
@@ -20,17 +21,48 @@ def start(update, context):
     # Send the message with the custom keyboard
     update.message.reply_text("Hello there🙋‍♂️\nSend me any YouTube video link🔗, and wait for the magic to happen🔮...\n\nYou can also use the following commands:", reply_markup=reply_markup)
 
-def get_dash_manifest_url(update, context):
-    """Handler function for the /dash_manifest_url command"""
+def get_dash_manifest_url_from_youtube(video_url):
+    try:
+        ydl_opts = {'format': 'best', 'quiet': True}
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(video_url, download=False)
+            dash_manifest_url = info['url']
+            return dash_manifest_url
+    except Exception as e:
+        print("Error fetching Dash Manifest URL:", str(e))
+        return None
 
-    # Get the YouTube video link from the user
+def get_dash_manifest_url(update: telegram.Update, context: CallbackContext):
+    """Handler function for the YouTube video link input"""
     video_url = update.message.text
 
-    # Get the dash manifest URL for the video
+    # You can fetch the Dash Manifest URL using the provided function
     dash_manifest_url = get_dash_manifest_url_from_youtube(video_url)
 
-    # Send the dash manifest URL to the user
-    update.message.reply_text(dash_manifest_url)
+    if dash_manifest_url:
+        # Save the Dash Manifest URL to the context for later use
+        context.user_data['dash_manifest_url'] = dash_manifest_url
+
+        # Move to the QUALITY_SELECTION state and ask the user to select the desired quality
+        update.message.reply_text("Select your desired quality and enjoy ✨", reply_markup=ReplyKeyboardMarkup([['/27p', '/45p', '/90p'], ['/144p', '/240p', '/360p'], ['/480p', '/720p', '/1080p']], resize_keyboard=True, one_time_keyboard=True))
+
+        return QUALITY_SELECTION
+    else:
+        update.message.reply_text("Sorry, I couldn't fetch the Dash Manifest URL for the provided YouTube video link.")
+        return ConversationHandler.END
+
+def select_quality(update: telegram.Update, context: CallbackContext):
+    """Handler function for the quality selection"""
+    selected_quality = update.message.text
+
+    # Get the saved Dash Manifest URL from the context
+    dash_manifest_url = context.user_data.get('dash_manifest_url')
+
+    # You can use the selected quality and the saved Dash Manifest URL to perform further actions
+    # For this example, I'll just reply with the selected quality and the Dash Manifest URL and end the conversation.
+    update.message.reply_text(f"Enjoy your video in {selected_quality} ✨\nManifest URL: {dash_manifest_url}", reply_markup=ReplyKeyboardRemove())
+
+    return ConversationHandler.END
 
 def help(update, context):
     """Handler function for the /help command"""
@@ -51,9 +83,9 @@ def main():
 
     # Create a conversation handler to handle the quality selection flow
     quality_selection_handler = ConversationHandler(
-        entry_points=[MessageHandler(Filters.regex(r"(http(s)?://)?((w){3}.)?youtu(be|.be)?(\.com)?/.+"), get_dash_manifest_url)],
+        entry_points=[MessageHandler(Filters.regex(r".*youtu(?:be\.com|\.be).*"), get_dash_manifest_url)],
         states={
-            QUALITY_SELECTION: [MessageHandler(Filters.regex(r"^\/[1-9][0-9]*p$"), get_dash_manifest_url)]
+            QUALITY_SELECTION: [MessageHandler(Filters.regex(r"^(\/[1-9][0-9]*p)$"), select_quality)]
         },
         fallbacks=[],
         allow_reentry=True
